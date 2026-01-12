4d1f00dc76c3cb55329c991ab1d03ce364901581
Kn4ughty <77908661+Kn4ughty@users.noreply.github.com>
2026-01-13 10:54:00 +1100

    Use stdlib from bytes functions

--- src/lib.rs
+++ src/lib.rs
@@ -311,62 +311,29 @@ impl MimeSearcher {
 impl MimeCacheHeader {
     fn read_header(input: &[u8; 40]) -> MimeCacheHeader {
         MimeCacheHeader {
-            major_version: u8x2_u16(input[0..2].try_into().unwrap()),
-            minor_version: u8x2_u16(input[2..4].try_into().unwrap()),
-            alias_list_offset: u8x4_u32(input[4..8].try_into().unwrap()),
-            parent_list_offset: u8x4_u32(input[8..12].try_into().unwrap()),
-            literal_list_offset: u8x4_u32(input[12..16].try_into().unwrap()),
-            reverse_suffix_tree_offset: u8x4_u32(input[16..20].try_into().unwrap()),
-            glob_list_offset: u8x4_u32(input[20..24].try_into().unwrap()),
-            magic_list_offset: u8x4_u32(input[24..28].try_into().unwrap()),
-            namespace_list_offset: u8x4_u32(input[28..32].try_into().unwrap()),
-            icons_list_offset: u8x4_u32(input[32..36].try_into().unwrap()),
-            generic_icons_list_offset: u8x4_u32(input[36..40].try_into().unwrap()),
+            major_version: u16::from_be_bytes(input[0..2].try_into().unwrap()),
+            minor_version: u16::from_be_bytes(input[2..4].try_into().unwrap()),
+            alias_list_offset: u32::from_be_bytes(input[4..8].try_into().unwrap()),
+            parent_list_offset: u32::from_be_bytes(input[8..12].try_into().unwrap()),
+            literal_list_offset: u32::from_be_bytes(input[12..16].try_into().unwrap()),
+            reverse_suffix_tree_offset: u32::from_be_bytes(input[16..20].try_into().unwrap()),
+            glob_list_offset: u32::from_be_bytes(input[20..24].try_into().unwrap()),
+            magic_list_offset: u32::from_be_bytes(input[24..28].try_into().unwrap()),
+            namespace_list_offset: u32::from_be_bytes(input[28..32].try_into().unwrap()),
+            icons_list_offset: u32::from_be_bytes(input[32..36].try_into().unwrap()),
+            generic_icons_list_offset: u32::from_be_bytes(input[36..40].try_into().unwrap()),
         }
     }
 }
 
-fn u8x2_u16(input: &[u8; 2]) -> u16 {
-    ((input[0] as u16) << 8) | input[1] as u16
-}
-
-// fn get_u16_panics(data: &[u8], index: usize) -> u16 {
-//     u8x2_u16(data[index..index + 2].try_into().unwrap())
-// }
-
-fn u8x4_u32(input: &[u8; 4]) -> u32 {
-    ((input[0] as u32) << 24)
-        | ((input[1] as u32) << (16))
-        | ((input[2] as u32) << (8))
-        | input[3] as u32
-}
-
 /// Panics all the time
 fn get_u32_panics(data: &[u8], index: usize) -> u32 {
-    u8x4_u32(data[index..index + 4].try_into().unwrap())
+    u32::from_be_bytes(data[index..index + 4].try_into().unwrap())
 }
 
 #[cfg(test)]
 mod test {
     use super::*;
-
-    #[test]
-    fn u8x2_u16_test() {
-        assert_eq!(u8x2_u16(&[0, 1]), 1);
-        assert_eq!(u8x2_u16(&[0, 2]), 2);
-        assert_eq!(u8x2_u16(&[1, 0]), 256);
-        assert_eq!(u8x2_u16(&[1, 1]), 257);
-        assert_eq!(u8x2_u16(&[122, 144]), 31376);
-        assert_eq!(u8x2_u16(&[255, 255]), u16::MAX);
-    }
-
-    #[test]
-    fn u8x4_u32_test() {
-        assert_eq!(u8x4_u32(&[0, 0, 0, 1]), 1);
-        assert_eq!(u8x4_u32(&[0, 0, 1, 0]), 256);
-        assert_eq!(u8x4_u32(&[0, 0, 122, 144]), 31376);
-        assert_eq!(u8x4_u32(&[255, 255, 255, 255]), u32::MAX);
-    }
 
     #[test]
     fn get_icon_for_mimetype() {
